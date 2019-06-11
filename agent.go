015e192375198e1f7705e69e685d82aab91a301d
udkumar <udkumar@hotmail.com>
2019-06-12 03:56:36 +0530

    collecting all function here

---
+++ agent.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/go_windows_agent/cpu"
+	"github.com/go_windows_agent/memory"
+	"github.com/go_windows_agent/disk"
+	"github.com/go_windows_agent/net"
+	"github.com/go_windows_agent/host"
+	"github.com/go_windows_agent/winservices"
+)
+
+func main() {
+
+	
+	
+}
