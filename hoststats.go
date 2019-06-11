ff65065465d7de6f05e9065e7e5dbce21469041e
udkumar <udkumar@hotmail.com>
2019-06-12 03:56:37 +0530

    file name changes

---
+++ hoststats.go
@@ -0,0 +1,15 @@
+package host
+
+import (
+    "fmt"
+
+    "github.com/shirou/gopsutil/host"
+)
+
+func main() {
+    infoStat, _ := host.Info()
+
+    //almost every return value is a struct
+    fmt.Printf("All Host info: ", infoStat)
+    
+}
