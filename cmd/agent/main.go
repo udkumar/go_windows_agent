688492034e9d89907db52f0cf416f6c33b029cef
udkumar <udkumar@hotmail.com>
2019-06-12 10:41:17 +0530

    changed import path for package

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -3,12 +3,7 @@ package main
 import (
 	"fmt"
 
-	"github.com/go_windows_agent/cpu"
-	"github.com/go_windows_agent/memory"
-	"github.com/go_windows_agent/disk"
-	"github.com/go_windows_agent/net"
-	"github.com/go_windows_agent/host"
-	"github.com/go_windows_agent/winservices"
+	"github.com/go_windows_agent/windows_agent"
 )
 
 func main() {
