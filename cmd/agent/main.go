4e83a20396bcbc85fd24dfee94c147abd8d14dd9
Minhaz Ahmed Syrus <msyrus@gmail.com>
2019-06-12 13:39:02 +0600

    Request stats every 20 seconds

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"mime"
 	"net/http"
+	"time"
 
 	windowsagent "github.com/udkumar/go_windows_agent/windows_agent"
 )
@@ -16,25 +17,29 @@ func main() {
 	flag.Parse()
 	reqURL := flag.Arg(0)
 
-	cpuInfo, _ := windowsagent.CpuStats()
-	fmt.Println("cpu info", cpuInfo)
+	for {
+		cpuInfo, _ := windowsagent.CpuStats()
+		fmt.Println("cpu info", cpuInfo)
 
-	if reqURL != "" {
-		body := map[string]interface{}{
-			"cpuInfo": cpuInfo,
+		if reqURL != "" {
+			body := map[string]interface{}{
+				"cpuInfo": cpuInfo,
+			}
+
+			buf := &bytes.Buffer{}
+			if err := json.NewEncoder(buf).Encode(body); err != nil {
+				fmt.Println("Failed to encode", err)
+				return
+			}
+
+			resp, err := http.Post(reqURL, mime.TypeByExtension(".json"), buf)
+			if err != nil {
+				fmt.Println("Failed to request", err)
+			} else {
+				fmt.Println("Response", resp.StatusCode)
+			}
 		}
 
-		buf := &bytes.Buffer{}
-		if err := json.NewEncoder(buf).Encode(body); err != nil {
-			fmt.Println("Failed to encode", err)
-			return
-		}
-
-		resp, err := http.Post(reqURL, mime.TypeByExtension(".json"), buf)
-		if err != nil {
-			fmt.Println("Failed to request", err)
-		} else {
-			fmt.Println("Response", resp.StatusCode)
-		}
+		time.Sleep(20 * time.Second)
 	}
 }
