007be5629eb910b7d903cbe1939d6388de437104
Dave Augustus <davetweetlive@gmail.com>
2023-04-28 09:20:31 +0530

    create service
    Signed-off-by: Dave Augustus <davetweetlive@gmail.com>

--- run.ps1
+++ run.ps1
@@ -1,3 +1,6 @@
+Set-ExecutionPolicy RemoteSigned
+
+
 # Check if Nmap is installed 
 if (!(Get-Command "nmap" -ErrorAction SilentlyContinue)) { 
 
@@ -17,3 +20,45 @@ if (!(Get-Command "nmap" -ErrorAction SilentlyContinue)) {
     
 # Test Nmap installation 
 nmap -V 
+
+# Set the current working directory to the location of the script
+Set-Location $PSScriptRoot
+
+# Build the binary from the Go source code
+go build -o .\main.exe .\cmd\main.go
+
+# Define variables
+$serviceName = "GoAgent"
+$displayName = "GoAgent"
+$description = "My custom service"
+$binaryPath = Join-Path $PSScriptRoot "main.exe"
+
+# Check if the service exists
+if (Get-Service -Name $serviceName -ErrorAction SilentlyContinue) {
+    # Stop the service if it's running
+    if ((Get-Service -Name $serviceName).Status -eq "Running") {
+        Stop-Service -Name $serviceName
+    }
+
+    # Delete the service
+    & sc.exe delete $serviceName
+    Write-Host "The '$serviceName' service has been deleted."
+
+      # The service does not exist, so create it
+      Write-Host "Creating the '$displayName' service, pointing to executable '$binaryPath'"
+      New-Service -Name $serviceName -BinaryPathName $binaryPath -DisplayName $displayName -Description $description -StartupType Automatic
+  
+      # Start the service
+      Write-Host "Starting the '$displayName' service..."
+      # Start-Service -Name $serviceName
+
+}else {
+        # The service does not exist, so create it
+        Write-Host "Creating the '$displayName' service, pointing to executable '$binaryPath'"
+        New-Service -Name $serviceName -BinaryPathName $binaryPath -DisplayName $displayName -Description $description -StartupType Automatic
+    
+        # Start the service
+        Write-Host "Starting the '$displayName' service..."
+        # Start-Service -Name $serviceName
+}
+
