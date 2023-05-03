7ea9bad562189195d64f22075acffdebe4feddab
Dave Augustus <davetweetlive@gmail.com>
2023-05-03 20:52:30 +0530

    Auto install nmap
    Signed-off-by: Dave Augustus <davetweetlive@gmail.com>

--- run.ps1
+++ run.ps1
@@ -1,23 +1,13 @@
-Set-ExecutionPolicy RemoteSigned
+ # Check if Chocolatey is installed
+ if (!(Get-Command "choco.exe" -ErrorAction SilentlyContinue)) {
+    # Install Chocolatey
+    Set-ExecutionPolicy Bypass -Scope Process -Force
+    iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
 
+# Install Nmap using Chocolatey
+choco install nmap -y --force
 
-# Check if Nmap is installed 
-if (!(Get-Command "nmap" -ErrorAction SilentlyContinue)) { 
-
-    # Download the Nmap installer 
-    $installerPath = "$env:TEMP\nmap-7.92-setup.exe" 
-    Invoke-WebRequest -Uri "https://nmap.org/dist/nmap-7.92-setup.exe" -OutFile $installerPath 
-    
-    # Run the installer 
-    Start-Process -FilePath $installerPath -Wait 
-
-    # Add Nmap to the PATH environment variable 
-    $nmapPath = "C:\Program Files (x86)\Nmap" 
-    $env:Path += ";$nmapPath" 
-    [Environment]::SetEnvironmentVariable("Path", $env:Path, [EnvironmentVariableTarget]::Machine) 
-    
-} 
-    
 # Test Nmap installation 
 nmap -V 
 
