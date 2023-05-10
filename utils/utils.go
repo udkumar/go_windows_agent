dcf29f771b96f4731ab157df5417ab0f0fdceeff
Dave Augustus <davetweetlive@gmail.com>
2023-05-10 20:34:24 +0530

    Public ip/mac address
    Signed-off-by: Dave Augustus <davetweetlive@gmail.com>

--- utils/utils.go
+++ utils/utils.go
@@ -1,8 +1,14 @@
 package utils
 
 import (
+	"errors"
+	"fmt"
+	"io/ioutil"
 	"net"
+	"net/http"
 	"os"
+
+	"github.com/sirupsen/logrus"
 )
 
 func GetPrivateIPAddress() (string, error) {
@@ -19,4 +25,53 @@ func GetPrivateIPAddress() (string, error) {
 // GetWorkingDir get the present working directory
 func GetWorkingDir() (string, error) {
 	return os.Getwd()
+}
+
+func GetPublicIP() (string, error) {
+	resp, err := http.Get("https://ifconfig.co/ip")
+	if err != nil {
+		logrus.Errorf("cannot execute API, error: %+v", err)
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	ip, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		logrus.Errorf("cannot read the IP, error: %+v", err)
+		return "", err
+	}
+
+	return string(ip), nil
+}
+
+func GetMacAddresses() (string, error) {
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		logrus.Errorf("cannot check N/W interface, error: %v", err)
+		return "", err
+	}
+
+	for _, iface := range interfaces {
+		// Skip loopback and non-up interfaces
+		if iface.Flags&net.FlagLoopback != 0 || iface.Flags&net.FlagUp == 0 {
+			continue
+		}
+
+		addrs, err := iface.Addrs()
+		if err != nil {
+			logrus.Errorf("cannot get unicast interface, error: %v", err)
+			return "", err
+		}
+
+		for _, addr := range addrs {
+			if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+				hwAddr := iface.HardwareAddr
+				fmt.Printf("MAC address: %s\n", hwAddr.String())
+				return hwAddr.String(), nil
+			}
+		}
+	}
+
+	fmt.Println("MAC address not found")
+	return "", errors.New("MAC address not found")
 }
